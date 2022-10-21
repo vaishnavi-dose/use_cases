b04e666b057a763cdba6142b57db33bd2cc7e059
vaishnavi-dose <114053692+vaishnavi-dose@users.noreply.github.com>
2022-10-21 18:05:39 +0530

    committing to "insurance usecase 21 oct"

--- insurance/insurance/src/main/java/com/example/policy/insurance/SampleController.java
+++ insurance/insurance/src/main/java/com/example/policy/insurance/SampleController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController 
 {
+	// this is for showing only one client data
 	@Autowired
 	private InsuranceRepo ir;
 	@GetMapping("/detailsc1") 
@@ -18,11 +20,34 @@ public class SampleController
 		return ir.getjoin(1);
 	}
 	
- @GetMapping("/detailsc3") 
+	@GetMapping("/detailsc3") 
 	
 	public List<InsuranceEntity> join3()
 	{
 		return ir.getjoin(3);
+	}
+	
+	// This is for discount...to display discount value
+	@GetMapping("/discount/{ten}/{premium}")
+	public int discount(@PathVariable String ten, @PathVariable int premium) 
+	{
+		if (ten.equals("morethan5"))
+		{
+			int x = (premium*12)/100;
+			premium = premium-x;
+			return premium;
+		}
+		if (ten.equals("equalsto5"))
+		{
+			int x = (premium*10)/100;
+			premium = premium-x;
+			return premium;
+		}
+		else if (ten.equals("lessthan5"))
+		{
+			return premium;
+		}
+		return 0;
 	}
 }
 
